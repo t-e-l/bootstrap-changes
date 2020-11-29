bef97cac5ffa8efde431c7f6c6eccfca41c98b54
SealedJoy <sealedjoy@gmail.com>
2020-11-29 19:22:50 +0000

    startup.sh changes

--- tel/.tel/scripts/startup.sh
+++ tel/.tel/scripts/startup.sh
@@ -2,9 +2,9 @@
 # TEL Startup file
 # these commands are ran only once when a session starts
 # this file will be replaced with each update so modifications are not recommended here
-clear
-CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
-echo -n "\e[4mLoading Things\e[0m" $'\r'
+source tel-helpers
+log "Loading Things"
+#echo -n "\e[4mLoading Things\e[0m" $'\r'
 sleep 0.1 # sleeps so line updates are actually visible to user
 #echo -n "reading user configs..                            " $'\r'
 #source ~/.tel/scripts/readconfigs.sh && echo -ne "all configs sourced ${CHECK_MARK}                " $'\r'
@@ -12,42 +12,44 @@ sleep 0.1
 
 # Handle motd hints system if user changes config option
 if  [ $MOTD_HINTS == "true" ] ; then # backup user motd and restore if hints disabled
-	[[ ! -f ~/../usr/etc/.motd.bak ]] && cp -f ~/../usr/etc/motd ~/../usr/etc/.motd.bak && echo -ne "backed up and enabled motd hints system ${CHECK_MARK}         " $'\r' && sleep 1
+	[[ ! -f ~/../usr/etc/.motd.bak ]] && cp -f ~/../usr/etc/motd ~/../usr/etc/.motd.bak && log "Backed up and enabled motd hints at startup ${CHECK_MARK}" && sleep 1
+		#echo -ne "backed up and enabled motd hints system ${CHECK_MARK}         " $'\r' && sleep 1
 	cp -f ~/../usr/etc/motd_hints ~/../usr/etc/motd && ~/.tel/scripts/hints.sh
 else
-	[[ -f ~/../usr/etc/.motd.bak ]] && mv -f ~/../usr/etc/.motd.bak ~/../usr/etc/motd && echo -ne "restored user motd ${CHECK_MARK}         " $'\r' && sleep 1
+	[[ -f ~/../usr/etc/.motd.bak ]] && mv -f ~/../usr/etc/.motd.bak ~/../usr/etc/motd && log "Restored user motd ${CHECK_MARK}" && sleep 1
+		#echo -ne "restored user motd ${CHECK_MARK}         " $'\r' && sleep 1
 fi
 
 if [ $SSH_SERVER == "true" ] ; then
-	echo -n "launching ssh server - SECURITY WARNING!  " $'\r'
+	warn "launching ssh server  "
 	sleep 0.2
 	sshd
-	echo -ne "launched ssh server ${CHECK_MARK}         " $'\r'
+	warn "launched ssh server ${CHECK_MARK}"
 	sleep 2.2
 fi
 
 if [ "$NOTIFICATIONS_ENABLED" == "true" ] ; then
-	echo -n "launching notification daemon              " $'\r'
+	log "launching notification daemon"
 	nohup ~/.tel/scripts/get_notifications.py > /dev/null 2>&1 &
-	echo -ne "launched notification daemon ${CHECK_MARK}" $'\r'
+	log "launched notification daemon ${CHECK_MARK}"
 fi
 
 if [ "$STARTUP_ANIMATION_ENABLED" == "true" ] ; then
-	echo -n 'launching animation                         ' $'\r'
+	log 'launching animation'
 	#tmux new-window -n '$ANIMATION_WINDOW_NAME' 'python ~/.tel/scripts/animation.py'
 	python ~/.tel/scripts/animation.py
-	echo -ne "launched python animation ${CHECK_MARK}    " $'\r'
+	log "launched python animation ${CHECK_MARK}"
 fi
 
 if [ "$STATUS_WINDOW_ENABLED" == "true" ] ; then
 	running=$(pgrep -f status_manager.py)
 	if [ -z "$running" ] ; then
-		echo -n 'launching status manager                    ' $'\r'
+		log 'launching status manager'
 		nohup ~/.tel/scripts/status_manager/toggle_ui.sh > /dev/null 2>&1 &
-		echo -ne "launched status manager ${CHECK_MARK}      " $'\r'
+		log "launched status manager ${CHECK_MARK}"
 	fi
 fi
 
-echo -ne "Ready!      ${CHECK_MARK}                " $'\r'
+log "Ready!      ${CHECK_MARK}"
 sleep 0.1
 exit 0
