13e9e11a15c59330412c8d2f627a2874a3e69a61
SealedJoy <sealedjoy@gmail.com>
2020-12-03 02:45:14 +0000

    change default modules

--- tel/.tel/configs/notifications.sh
+++ tel/.tel/configs/notifications.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ############### TEL Notifications Configuration ###########
-#    Status module at ~/.tel/status/$$-notifications.sh   #
+#   Status module at ~/.tel/status/**/*-notifications.sh  #
 #                   Restart to see changes             	  #
 ###########################################################
 
@@ -9,6 +9,6 @@
 export NOTIFICATIONS_ENABLED=false #enable notification daemon (requires api permissions) [true/false]
 export COLOR_APP_TITLES=true  #apply color to package titles in notification display [true/false]
 export NOTIFICATIONS_CHECK_DELAY=2  #Check for new notifs every [seconds]
-export NOTIFICATIONS_DISPLAY_TYPE=scroll # in the status bar module, scroll through each notification or just show latest [scroll/latest]
+export NOTIFICATIONS_DISPLAY_TYPE=latest # in the status bar module, scroll through each notification or just show latest [scroll/latest]
 
 ### NOTE - EXTRA OPTIONS AVAILIBLE IN ~/.tel/configs/notifications/
