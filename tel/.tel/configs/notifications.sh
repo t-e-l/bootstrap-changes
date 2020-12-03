73f16f0d4e005ae677b37280ff7629573c634752
SealedJoy <sealedjoy@gmail.com>
2020-12-03 02:59:37 +0000

    fix notifs

--- tel/.tel/configs/notifications.sh
+++ tel/.tel/configs/notifications.sh
@@ -6,7 +6,7 @@
 ###########################################################
 
 # Enable / Disable Notifications completely
-export NOTIFICATIONS_ENABLED=false #enable notification daemon (requires api permissions) [true/false]
+export NOTIFICATIONS_ENABLED=true #enable notification daemon (requires api permissions) [true/false]
 export COLOR_APP_TITLES=true  #apply color to package titles in notification display [true/false]
 export NOTIFICATIONS_CHECK_DELAY=2  #Check for new notifs every [seconds]
 export NOTIFICATIONS_DISPLAY_TYPE=latest # in the status bar module, scroll through each notification or just show latest [scroll/latest]
