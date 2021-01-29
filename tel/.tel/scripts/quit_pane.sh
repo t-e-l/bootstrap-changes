d13896e1cbaa3e4f006565005885756387c217a0
SealedJoy <sealedjoy@gmail.com>
2021-01-29 12:29:04 +0000

    add script to prevent last window being closed with tel +q

--- tel/.tel/scripts/quit_pane.sh
+++ tel/.tel/scripts/quit_pane.sh
@@ -1,3 +1,6 @@
 if [ $(tmux list-panes | wc -l) -gt 1 ] ; then
+	tmux display-message "[TEL] Quit pane..."
 	tmux kill-pane
+else
+	tmux display-message "[TEL] use command 'exit' to exit from tmux env"
 fi
