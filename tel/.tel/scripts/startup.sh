3ed47d848d1adac09efd8e544c7012622e124a62
SealedJoy <sealedjoy@gmail.com>
2021-02-16 14:05:27 +0000

    add info when setting background / themestuff on startup

--- tel/.tel/scripts/startup.sh
+++ tel/.tel/scripts/startup.sh
@@ -18,8 +18,12 @@ sleep 0.1
 
 # Set wallpaper if flagged by tel-theme
 if [ -f ~/.tel/theme/.flag ] ; then
+	log_replace_last 'implementing theme changes'
 	termux-wallpaper -f ~/.tel/theme/wallpaper/*.*
 	rm -rf ~/.tel/theme/.flag
+	log_replace_last "implemented theme changes ${CHECK_MARK}"
+	echo
+
 fi
 
 
