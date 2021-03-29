e56b5e9f476c9c3c641a282905614863e21eda11
SealedJoy <sealedjoy@gmail.com>
2021-03-29 17:14:21 +0100

    default sett to ddg

--- tel/.tel/configs/search.sh
+++ tel/.tel/configs/search.sh
@@ -3,6 +3,7 @@
 #                   Restart to see changes                #
 ###########################################################
 # find and edit the trusted search url list and list of search bangs inside ~/.tel/configs/search/ 
-export SEARCH_LANG='en-GB' #search results format returned in iso formatted country list, search wikipedia iso formats to find your correct country/language code [must not be empty]
-
+export SEARCH_LANG='en_GB' #search results format returned in iso formatted country list, search wikipedia iso formats to find your correct country/language code [must not be empty]
+export SEARCH_LOCATION='' #Get results specific to your country, iso code, eg ['gb','de'...]  [can be empty]
+export SEARCH_PROVIDER='duckduckgo' # which search engine should tel-search utilise [qwant, duckduckgo, searx, metager]
 # See more config options in ~/.tel/configs/search/
